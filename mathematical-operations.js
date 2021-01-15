4e3fb35c26e16aa9ca41e6c60cdbb26486b420ae
stsharin <stssharin@gmail.com>
2021-01-16 03:25:14 +0600

    second commit

---
+++ mathematical-operations.js
@@ -0,0 +1,33 @@
+var price1 = 12;
+var price2 = 15;
+
+var total = (price1 + price2);
+console.log(total);
+
+var minus = (price2 - price1);
+console.log(minus);
+
+var mult = (price1 * price2);
+console.log(mult);
+
+var div = (price1 / price2);
+console.log(div);
+
+var remainder = (price2 % price1);
+console.log(remainder);
+
+remainder++;
+// remainder--;
+console.log(remainder);
+
+var fname = "Justin";
+var lname = "Bieber";
+var fullname = "Full Name is: " + fname +" "+ lname;
+console.log(fullname);
+
+var one = "99.5";
+
+var two = 15;
+
+console.log(one+two);
+
